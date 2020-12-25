07ddce6f1a108f0e55e0a1f78cd17fd17fe12c60
Andrew Herasimenko <fylhsqvh@gmail.com>
2020-12-26 01:16:37 +0200

    Ready application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
+import React, { useEffect, useState, useRef } from 'react';
+import { interval, of, Subject } from "rxjs";
+import {
+  scan,
+  startWith,
+  switchMap,
+  filter
+} from "rxjs/operators";
+import moment from 'moment';
 import './App.css';
 
+const action$ = new Subject();
+
+const observable$ = action$.pipe(
+  switchMap(x => {
+    if(x === "pause") {
+      return of("pause")
+    } else if(x === "stop") {
+      return of(0, "pause");
+    } else {
+      return interval(1000).pipe(
+        startWith(x),
+        scan(number => number + 1)
+      );
+    }
+  }
+)).pipe(filter(x => x !== "pause"));
+
+const formatTime = (seconds) => {
+  return moment("00:00:00", "HH:mm:ss").add(seconds, "s").format("HH:mm:ss");
+}
+
 function App() {
+  const [time, setTime] = useState(0);
+  const [isGoing, setIsGoing] = useState(false);
+  const dblClick = useRef(null);
+
+  useEffect(() => {
+    const subscription = observable$.subscribe(setTime);
+    return () => {
+      subscription.unsubscribe();
+    }
+  }, []);
+
+  const onStartButtonHandler = () => {
+    if(isGoing) {
+      action$.next("stop");
+    } else {
+      action$.next(time);
+    }
+      setIsGoing(prev => !prev);
+  }
+
+  const onResetButtonHandler = () => {
+    action$.next(0);
+    setIsGoing(true);
+  }
+
+  const onWaitButtonHandler = (event) => {
+    const target = event.currentTarget;
+
+    const clickListener = () => {
+      action$.next("pause");
+      setIsGoing(false);
+      clearTimeout(dblClick.current);
+      target.removeEventListener('click', clickListener);
+      dblClick.current = null;
+    }
+
+    if(!dblClick.current) {
+      target.addEventListener('click', clickListener);
+    
+      dblClick.current = setTimeout(() => {
+        target.removeEventListener('click', clickListener);  
+        dblClick.current = null;
+      }, 300)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>StopWatch App</h2>
+      <h1>
+        {
+          formatTime(time)
+        }
+      </h1>
+      <button onClick={onWaitButtonHandler}>
+        Wait
+      </button>
+      <button className="start_button" onClick={onStartButtonHandler}>
+        {
+          isGoing ? "Stop" : "Start"
+        }
+      </button>
+      <button onClick={onResetButtonHandler}>
+        Reset
+      </button>
     </div>
   );
 }
